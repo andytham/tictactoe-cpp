ec81c8b29595abd91fa7b60bc26f5e6477df5528
andy <thamandyy@gmail.com>
2019-03-28 00:56:43 -0400

    Kept function, changed it to use reference

--- tictactoe-cpp/main.cpp
+++ tictactoe-cpp/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <unordered_map>
 template <size_t rows, size_t cols>
-bool isWinner(int(&solution)[rows][cols], std::unordered_map<int, std::string> board, std::string currentPlayer) {
+bool isWinner(int (&solution)[rows][cols], std::unordered_map<int, std::string> board, std::string currentPlayer) {
 	std::cout << "solution check" << std::endl;
 	for (auto i = 0; i < 8; i++) {
 		if (board[solution[i][0]] == currentPlayer && board[solution[i][0]] == board[solution[i][1]] && board[solution[i][0]] == board[solution[i][2]]) {
@@ -12,6 +12,11 @@ bool isWinner(int(&solution)[rows][cols], std::unordered_map<int, std::string> b
 	}
 	std::cout << "No solution" << std::endl;
 	return false;
+}
+void placePiece(int pos, std::string currentPlayer, std::unordered_map<int, std::string>& board){
+	board[pos] = currentPlayer;
+	std::cout << "place piece is ran" << pos << currentPlayer;
+	return;
 }
 void printBoard(std::unordered_map<int, std::string> board) {
 	std::cout << board[1] << "|" << board[2] << "|" << board[3] << std::endl;
@@ -41,13 +46,12 @@ int main() {
 		std::cin >> input;
 		while (true) {
 			if (input > 0 && input < 10) {
-				board[input] = currentPlayer;
+				placePiece(input, currentPlayer, board);
 				break;
 			}
 			std::cout << "Enter valid spot: " << std::endl;
 			std::cin >> input;
 		}
-		placePiece(input, currentPlayer, board);
 		std::cout << currentPlayer << input << std::endl;
 		if (isWinner(solution, board, currentPlayer)) {
 			std::cout << "Winner is " << currentPlayer << std::endl;
