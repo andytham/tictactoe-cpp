ba81a42fd58d2c4f860d164314775fbb4408ca21
andy <thamandyy@gmail.com>
2019-03-28 00:52:30 -0400

    Actually place piece in game logic don't need extra function

--- tictactoe-cpp/main.cpp
+++ tictactoe-cpp/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <unordered_map>
 template <size_t rows, size_t cols>
-bool isWinner(int (&solution)[rows][cols], std::unordered_map<int, std::string> board, std::string currentPlayer) {
+bool isWinner(int(&solution)[rows][cols], std::unordered_map<int, std::string> board, std::string currentPlayer) {
 	std::cout << "solution check" << std::endl;
 	for (auto i = 0; i < 8; i++) {
 		if (board[solution[i][0]] == currentPlayer && board[solution[i][0]] == board[solution[i][1]] && board[solution[i][0]] == board[solution[i][2]]) {
@@ -13,8 +13,10 @@ bool isWinner(int (&solution)[rows][cols], std::unordered_map<int, std::string> 
 	std::cout << "No solution" << std::endl;
 	return false;
 }
-void placePiece(int pos, std::string currentPlayer, std::unordered_map<int, std::string> board){
-	board[pos] = currentPlayer;
+void printBoard(std::unordered_map<int, std::string> board) {
+	std::cout << board[1] << "|" << board[2] << "|" << board[3] << std::endl;
+	std::cout << board[4] << "|" << board[5] << "|" << board[6] << std::endl;
+	std::cout << board[7] << "|" << board[8] << "|" << board[9] << std::endl;
 	return;
 }
 int main() {
@@ -31,14 +33,15 @@ int main() {
 	{ 3, 6, 9 }
 	};
 	std::unordered_map<int, std::string> board;
-	board[1] = "X";
 	bool gameOver = false;
 	std::string currentPlayer = "X";
 	while (!gameOver) {
+		printBoard(board);
 		std::cout << "Enter next command" << std::endl;
 		std::cin >> input;
 		while (true) {
 			if (input > 0 && input < 10) {
+				board[input] = currentPlayer;
 				break;
 			}
 			std::cout << "Enter valid spot: " << std::endl;
